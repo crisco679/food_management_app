2b94db19c54b6fb8075b0c296fbcce6a08cbd9fb
christuttle <crunch.tuttle@gmail.com>
2016-06-18 15:21:02 -0500

    merges master and tuttle

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,9 @@
 var router = require('express').Router();
 
-
+router.get('/', function(req, res){
+  console.log('%*%*%*%*%*%*%*%*%%*', req);
+})
+router.post('/', function(req, res){
+  console.log('%$%$%$%$%$%%$%$%%$$', req);
+})
 module.exports = router;
