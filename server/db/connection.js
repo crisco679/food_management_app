67bc071007b6198a1198916b6f5d2abad822a366
TravisIngram <ingramtravis@gmail.com>
2016-06-17 23:35:22 -0500

    Add account schema to connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,7 @@
 var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/food_management';
 
-function initializeDB() {
+function initializeExpense() {
   pg.connect(connectionString, function(err, client, done) {
     if ( err ) {
       console.log('Error connecting to DB', err);
@@ -27,5 +27,31 @@ function initializeDB() {
   });
 }
 
+function initializeAccount() {
+  pg.connect(connectionString, function(err, client, done) {
+    if ( err ) {
+      console.log('Error connecting to DB', err);
+      process.exit(1);
+    } else {
+      var query = client.query('CREATE TABLE IF NOT EXISTS "account" (' +
+      'account_id serial PRIMARY KEY,' +
+      'username varchar(80) NOT NULL,' +
+      'password varchar(50) NOT NULL,' +
+      'email_address varchar(50));');
+
+      query.on('end', function() {
+        console.log('Successfully ensured Account schema exists.');
+        done();
+      });
+
+      query.on('error', function() {
+        console.log('Error creating Expense schema.' + err);
+        process.exit(1);
+      });
+    }
+  });
+}
+
 module.exports.connectionString = connectionString;
-module.exports.initializeDB = initializeDB;
+module.exports.initializeExpense = initializeExpense;
+module.exports.initializeAccount = initializeAccount;
